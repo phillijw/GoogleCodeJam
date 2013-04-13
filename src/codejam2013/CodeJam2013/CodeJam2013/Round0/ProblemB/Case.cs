9f4d93591153690fc96f4488fc93e9b4156e4276
Joe Phillips <phillijw@gmail.com>
2013-04-13 15:23:12 -0500

    Finished up ProblemB

--- src/codejam2013/CodeJam2013/CodeJam2013/Round0/ProblemB/Case.cs
+++ src/codejam2013/CodeJam2013/CodeJam2013/Round0/ProblemB/Case.cs
@@ -7,10 +7,57 @@ namespace CodeJam2013.Round0.ProblemB
 {
     static class Case
     {
-        public static string Run(string lawn)
+        public static string Run(string input)
         {
+            var inputArray = input.Split(' ');
+            int n = Int32.Parse(inputArray[0]);
+            int m = Int32.Parse(inputArray[1]);
+            var lawn = inputArray.ToList().GetRange(2, inputArray.Length - 2);
+            var lawnInts = lawn.Select(int.Parse).ToList();
 
-            return "test";
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (IsAccessible(lawnInts, m, n, j, i))
+                        continue;
+                    else
+                        return "NO";
+                }
+            }
+
+            return "YES";
+        }
+
+        private static bool IsAccessible(List<int> lawn, int m, int n, int x, int y)
+        {
+            return IsHorizontallyAccessible(lawn, m, n, x, y) || IsVerticallyAccessible(lawn, m, n, x, y);
+        }
+
+        private static bool IsHorizontallyAccessible(List<int> lawn, int m, int n, int x, int y)
+        {
+            //Is there any taller grass in this row?
+            var row = lawn.GetRange(y * m, m);
+            var accessible = row.Max() <= lawn[y*m+x];
+            return accessible;
+        }
+
+        private static bool IsVerticallyAccessible(List<int> lawn, int m, int n, int x, int y)
+        {
+            //Is there any taller grass in this column?
+            var col = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j=0; j < m; j++)
+                {
+                    if (j == x)
+                        col.Add(lawn[i * m + j]);
+                }
+            }
+
+            var accessible = col.Max() <= lawn[y * m + x];
+            return accessible;
         }
     }
 }
