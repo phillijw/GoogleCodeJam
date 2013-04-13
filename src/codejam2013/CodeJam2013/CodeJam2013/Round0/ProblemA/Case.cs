1d08173e6a1d15b6630511efa472f883466c1104
Joe Phillips <phillijw@gmail.com>
2013-04-13 02:50:52 -0500

    Beginning to work on ProblemA

---
+++ src/codejam2013/CodeJam2013/CodeJam2013/Round0/ProblemA/Case.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeJam2013.Round0.ProblemA
+{
+    static class Case
+    {
+        public static string Run(string board)
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                //board.Substring(i, 4).
+            }
+            return board;
+        }
+    }
+}
--- src/codejam2013/CodeJam2013/CodeJam2013/Round0/ProblemA/ProblemA.cs
+++ src/codejam2013/CodeJam2013/CodeJam2013/Round0/ProblemA/ProblemA.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.IO;
+using System;
 
 namespace CodeJam2013.Round0.ProblemA
 {
@@ -9,6 +10,20 @@ namespace CodeJam2013.Round0.ProblemA
     {
         public string Run(string input)
         {
+            input = input.Replace("\r\n", "\n");
+            input = input.Replace("\n\n", "\n"); //Get rid of empty lines because they'll just get in the way
+            var lines = input.Split('\n').ToList();
+            var caseCount = Int32.Parse(lines[0]);
+            var cases = input.Substring(input.IndexOf("\n")).Replace("\n", "");
+
+            //Console.Write(cases);
+
+
+            for (int i = 0; i < caseCount; i++)
+            {
+                Console.WriteLine(Case.Run(cases.Substring(i*16, 16)));
+            }
+
             //Do nothing... so far
             return "test";
         }
